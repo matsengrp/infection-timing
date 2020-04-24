7fcfb3683da25006a597cdd7c60100f84257284f
Magdalena Russell <magdalenarussell23@gmail.com>
2020-04-24 13:13:36 -0600

    playing with GEE model and adding comments

--- Infant_Model/Infant_functions.R
+++ Infant_Model/Infant_functions.R
@@ -1,5 +1,6 @@
 ###
 #' Created on Tue April 20 2020
+#' 
 #'@author: magdalenarussell
 ###
 
@@ -8,59 +9,36 @@ library(ggplot2)
 library(caret)
 library(tidyverse)
 library(L1pack)
+library(gee)
 
 source("../common_regression.R")
 data = read.csv("../_ignore/AllRunsAvg.csv")
 
-#' Regression function for APD ~ Time by patient, fragment (from common_regression.R)
-regress_F3_1 <- function(df, pat){
-    df = data.table(df)
-    if (length(df[sample == pat]$avg_apd1) <= 1){
-        p = NA
-        slope_num = NA
-    } else {
-        modelobject = lm(df[sample == pat]$avg_apd1 ~ df[sample == pat]$time, na.action = na.exclude)
-        slope = coefficients(modelobject)[-1]
-        intercept = coefficients(modelobject)[-2]
-        slope_num = as.double(paste(slope[1]))
-        int_num = as.double(paste(intercept[1]))
-        f = summary(modelobject)$fstatistic
-        p = pf(f[1],f[2],f[3],lower.tail=F)
-        attributes(p) <- NULL
-    }
-    return(c(slope_num, int_num, p))
-}
 
-# Data cleaning, APD averaging, Position averaging
-data_clean <- function(data, impute){
+#' Data cleaning, APD averaging, Position averaging for training data
+#' 
+#' @param data: Dataframe containing training data with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`, `VL`
+#' 
+#' @return data_avg: Data.table containing the following columns: "sample", "time", "fragment", "vload","avg_apd1", "avg_apd5", "avg_apd10", "pos_start", "pos_end", and "actual_ti" (defined as time since infection with infection start as the midpoint of the third trimester)
+#' 
+data_clean <- function(data){
     data = data.table(data)
     data_avg = data[APD_1 != 'NA', .(mean(APD_1), mean(APD_5), mean(APD_10), mean(HXB2nt_start), mean(HXB2nt_end)), by = .(Sample, ActualTOI..year., Fragment, VL)]
     colnames(data_avg) = c("sample", "time", "fragment", "vload","avg_apd1", "avg_apd5", "avg_apd10", "pos_start", "pos_end")
-    if (impute == 'TRUE'){
-        data_avg_3 = data_avg[fragment == 'F3']
-        lm_df = NULL
-        for (pat in as.vector(unlist(unique(data_avg_3$sample)))){
-            sample = pat
-            apd_time_slope = regress_F3_1(data_avg_3, pat)[1]
-            apd_time_intercept = regress_F3_1(data_avg_3, pat)[2]
-            apd_time_p_val = regress_F3_1(data_avg_3, pat)[3] 
-            lm_df = rbind(lm_df, data.frame(sample, apd_time_slope, apd_time_intercept, apd_time_p_val))
-        }
-        both = merge(data_avg_3, lm_df)
-        both = both[, .(calc_infect_time = -(apd_time_intercept)/apd_time_slope), by = .(sample)]
-        both = unique(both)
-        data_avg = merge(data_avg, both, by = "sample")
-        data_avg = data_avg[calc_infect_time < -0.75, calc_infect_time := -0.125]
-        data_avg = data_avg[calc_infect_time > 1, calc_infect_time := -0.125]
-        data_avg$actual_ti = data_avg$time - data_avg$calc_infect_time
-    } else {
-        data_avg$actual_ti = data_avg$time +0.125
-    }
+    data_avg$actual_ti = data_avg$time +0.125
     return(data_avg)
 }
 
-create_model_lm <- function(data, cutoff, fragment, impute){
-    data_cleaned = data_clean(data, impute)
+#' Create lm regression (for indicated fragment and apd) with slope and nonzero y-intercept with apd as regressor using 11-fold cross validation
+#' 
+#' @param data: Dataframe containing training or testing data with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`, `VL`
+#' @param cutoff: apd cutoff (options: 1, 5, 10)
+#' @param fragment: sequencing region (options: 'F1', 'F2', 'F3')
+#' 
+#' @return lm regression object for indicated fragment and apd
+#' 
+create_model_lm <- function(data, cutoff, fragment){
+    data_cleaned = data_clean(data)
     
     # Subset data by indicated fragment
     if (fragment == "F1"){
@@ -84,8 +62,16 @@ create_model_lm <- function(data, cutoff, fragment, impute){
     return(get(paste0("model", cutoff)))
 }
 
-create_model_lm_origin <- function(data, cutoff, fragment, impute){
-    data_cleaned = data_clean(data, impute)
+#' Create lm regression (for indicated fragment and apd) with slope and zero y-intercept with apd as regressor using 11-fold cross validation
+#' 
+#' @param data: Dataframe containing training or testing data with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`, `VL`
+#' @param cutoff: apd cutoff (options: 1, 5, 10)
+#' @param fragment: sequencing region (options: 'F1', 'F2', 'F3')
+#' 
+#' @return lm regression object for indicated fragment and apd with zero y-intercept
+#' 
+create_model_lm_origin <- function(data, cutoff, fragment){
+    data_cleaned = data_clean(data)
     
     # Subset data by indicated fragment
     if (fragment == "F1"){
@@ -109,8 +95,16 @@ create_model_lm_origin <- function(data, cutoff, fragment, impute){
     return(get(paste0("model", cutoff)))
 }
 
-create_model_lad <- function(data, cutoff, fragment, impute){
-    data_cleaned = data_clean(data, impute)
+#' Create lad (least absolute deviation) regression (for indicated fragment and apd) with slope and nonzero y-intercept with apd as regressor (here, no 11-fold cross validation)
+#' 
+#' @param data: Dataframe containing training or testing data with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`, `VL`
+#' @param cutoff: apd cutoff (options: 1, 5, 10)
+#' @param fragment: sequencing region (options: 'F1', 'F2', 'F3')
+#' 
+#' @return lad regression object for indicated fragment and apd
+#' 
+create_model_lad <- function(data, cutoff, fragment){
+    data_cleaned = data_clean(data)
     
     # Subset data by indicated fragment
     if (fragment == "F1"){
@@ -130,8 +124,16 @@ create_model_lad <- function(data, cutoff, fragment, impute){
     return(get(paste0("model", cutoff)))
 }
 
-create_model_lad_origin <- function(data, cutoff, fragment, impute){
-    data_cleaned = data_clean(data, impute)
+#' Create lad (least absolute deviation) regression (for indicated fragment and apd) with slope and zero y-intercept with apd as regressor (here, no 11-fold cross validation)
+#' 
+#' @param data: Dataframe containing training or testing data with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`, `VL`
+#' @param cutoff: apd cutoff (options: 1, 5, 10)
+#' @param fragment: sequencing region (options: 'F1', 'F2', 'F3')
+#' 
+#' @return lad regression object for indicated fragment and apd with zero y-intercept
+#' 
+create_model_lad_origin <- function(data, cutoff, fragment){
+    data_cleaned = data_clean(data)
     
     # Subset data by indicated fragment
     if (fragment == "F1"){
@@ -151,68 +153,47 @@ create_model_lad_origin <- function(data, cutoff, fragment, impute){
     return(get(paste0("model", cutoff)))
 }
 
-create_model_lm_log <- function(data, cutoff, fragment, impute){
-    data_cleaned = data_clean(data, impute)
-    
-    # Subset data by indicated fragment
-    if (fragment == "F1"){
-        data_cleaned = data_cleaned[fragment == "F1"]
-    } else if (fragment == "F2"){
-        data_cleaned = data_cleaned[fragment == "F2"]
-    } else if (fragment == "F3"){
-        data_cleaned = data_cleaned[fragment == "F3"]
-    }
+#' Create lm regression (for indicated apd) with slope and nonzero y-intercept with apd and fragment as regressors using 11-fold cross validation
+#' 
+#' @param data: Dataframe containing training or testing data with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`, `VL`
+#' @param cutoff: apd cutoff (options: 1, 5, 10)
+#' 
+#' @return lm gee regression object for indicated apd
+#' 
+create_model_lm_gee <- function(data, cutoff, fragment = "all"){
+    data_cleaned = data_clean(data)
 
-    # Define training control (11-fold cross validation, repeated 3 times)
-    folds <- groupKFold(data_cleaned$sample, k = length(unique(data_cleaned$sample))) 
-    train.control <- trainControl(method = "cv", index = folds, savePredictions = "all", returnResamp = "all")
-    data_cleaned = data_cleaned[avg_apd1 == 0, avg_apd1:= NA]
-    data_cleaned = data_cleaned[avg_apd5 == 0, avg_apd5:= NA]
-    data_cleaned = data_cleaned[avg_apd10 == 0, avg_apd10:= NA]
+    # gee requires ordering/sorting by clustering variable
+    data_cleaned = data_cleaned[order(sample)]
+
     # Train the model
-    model1 <- train(actual_ti ~ log(avg_apd1), data = data_cleaned, method = "lm", trControl = train.control, na.action = na.pass)
-    model5 <- train(actual_ti ~ log(avg_apd5), data = data_cleaned, method = "lm", trControl = train.control, na.action = na.pass)
-    model10 <- train(actual_ti ~ log(avg_apd10), data = data_cleaned, method = "lm", trControl = train.control, na.action = na.pass)
-    
+    model1 <- gee(actual_ti ~ avg_apd1 + factor(fragment), data = data_cleaned, id = sample, na.action = na.omit)
+    model5 <- gee(actual_ti ~ avg_apd5 + factor(fragment), data = data_cleaned, id = sample, na.action = na.omit)
+    model10 <- gee(actual_ti ~ avg_apd10 + factor(fragment), data = data_cleaned, id = sample,na.action = na.omit)
+
     # Summarize the results
     return(get(paste0("model", cutoff)))
 }
 
-create_model_lm_poly <- function(data, cutoff, fragment, impute){
-    data_cleaned = data_clean(data, impute)
-    
-    # Subset data by indicated fragment
-    if (fragment == "F1"){
-        data_cleaned = data_cleaned[fragment == "F1"]
-    } else if (fragment == "F2"){
-        data_cleaned = data_cleaned[fragment == "F2"]
-    } else if (fragment == "F3"){
-        data_cleaned = data_cleaned[fragment == "F3"]
-    }
+# Create models for each fragment, apd, model type combination:
 
-    # Define training control (11-fold cross validation, repeated 3 times)
-    folds <- groupKFold(data_cleaned$sample, k = length(unique(data_cleaned$sample))) 
-    train.control <- trainControl(method = "cv", index = folds, savePredictions = "all", returnResamp = "all")
-
-    # Train the model
-    model1 <- train(actual_ti ~ poly(avg_apd1, 3, raw = TRUE), data = data_cleaned, method = "lm", trControl = train.control, na.action = na.pass)
-    model5 <- train(actual_ti ~ poly(avg_apd5, 3, raw = TRUE), data = data_cleaned, method = "lm", trControl = train.control, na.action = na.pass)
-    model10 <- train(actual_ti ~ poly(avg_apd10, 3, raw = TRUE), data = data_cleaned, method = "lm", trControl = train.control, na.action = na.pass)
-    
-    # Summarize the results
-    return(get(paste0("model", cutoff)))
-}
-
-for (frag in c('F1', 'F2', 'F3')){
-    for (cut in c(1, 5, 10)){
-        for (imp in c('FALSE', 'TRUE')){
-            assign(paste0('LM_model', cut, frag, '_', imp), create_model_lm(data, cut, frag, imp))
-            assign(paste0('LM_origin_model', cut, frag, '_', imp), create_model_lm_origin(data, cut, frag, imp))
-            assign(paste0('LAD_model', cut, frag, '_', imp), create_model_lad(data, cut, frag, imp))
-            assign(paste0('LAD_origin_model', cut, frag, '_', imp), create_model_lad_origin(data, cut, frag, imp))
-            #assign(paste0('LM_log_model', cut, frag, '_', imp), create_model_lm_log(data, cut, frag, imp))
-            #assign(paste0('LM_poly_model', cut, frag, '_', imp), create_model_lm_poly(data, cut, frag, imp))
-        }
+for (cut in c(1, 5, 10)){
+    assign(paste0('LM_GEE_model', cut), create_model_lm_gee(data, cut))
+    for (frag in c('F1', 'F2', 'F3')){
+        assign(paste0('LM_model', cut, frag), create_model_lm(data, cut, frag))
+        assign(paste0('LM_origin_model', cut, frag), create_model_lm_origin(data, cut, frag))
+        assign(paste0('LAD_model', cut, frag), create_model_lad(data, cut, frag))
+        assign(paste0('LAD_origin_model', cut, frag), create_model_lad_origin(data, cut, frag))
     }
 }
+
+
+#gee_model_p_val <- function(apd){
+#    model = get(paste0('LM_GEE_model', apd))    
+#    # Calculate regressor p-values  
+#    beta = model$coef # Extract coefficients
+#    se = sqrt(diag(model$robust.variance)) # Extract covariance matrix, take diagonal elements, and square root
+#    p = 2*(1-pnorm(abs(beta)/se)) 
+#    return(p)
+#}
 
--- Infant_Model/Infant_plotting.R
+++ Infant_Model/Infant_plotting.R
@@ -10,30 +10,32 @@ library(ggfortify)
 source("Infant_functions.R")
 source("Infant_predict.R")
 
-#' Creates plot title given input variables.
-
-title <- function(apd, x, y, type, impute) {
+#' Create plot title given apd cutoff, x and y variables, and regression type
+#' 
+#' @param apd: apd cutoff (options: 1, 5, 10)
+#' @param x: x variable to be plotted (so far, option is "ti")
+#' @param y: y variable to be plotted (so far, option is "eti")
+#' @param type: regression model type (options: "LM", "LM_origin", "LAD", "LAD_origin")
+#' 
+title <- function(apd, x, y, type) {
     if (y == "eti" && x == "ti"){
-        if (impute =='FALSE'){
-            title_name = paste0("Average ETI vs. Actual TI for APD-",apd, " using ", type, " Infant Model with impute ", impute) #, '\n', "Actual TI is halfway through third trimester for all")
-        } else if (impute == 'TRUE'){
-            title_name = paste0("Average ETI vs. Actual TI for APD-",apd, " using ", type, " Infant Model with impute ", impute) #, '\n', "Actual TI is estimated using APD change over time")
-        }
+        title_name = paste0("Average ETI vs. Actual TI for APD-",apd, " using ", type, " Infant Model")
     } else {
         print("ERROR: incompatible x and y variables")
     }   
     return(title_name)
 }
 
-#' Creates plot file name given input variables. 
-
-file <- function(apd, x, y, type, impute) {
+#' Create file name given apd cutoff, x and y variables, and regression type
+#' 
+#' @param apd: apd cutoff (options: 1, 5, 10)
+#' @param x: x variable to be plotted (so far, option is "ti")
+#' @param y: y variable to be plotted (so far, option is "eti")
+#' @param type: regression model type (options: "LM", "LM_origin", "LAD", "LAD_origin")
+#' 
+file <- function(apd, x, y, type) {
     if (y == "eti" && x == "ti"){
-        if (impute =='FALSE'){
-            file_name = paste0("AverageETI_ActualTI_for_APD",apd, "_", type, "_Infant_Model_midpoint_actual_TI")
-        } else if (impute == 'TRUE'){
-            file_name = paste0("AverageETI_ActualTI_for_APD",apd,  "_", type, "_Infant_Model_imputed_actual_TI")
-        }
+        file_name = paste0("AverageETI_ActualTI_for_APD",apd, "_", type, "_Infant_Model_midpoint_actual_TI")
     } else {
         print("ERROR: incompatible x and y variables")
     }     
@@ -41,18 +43,29 @@ file <- function(apd, x, y, type, impute) {
 }
 
 #' Plot Average Estimated Time of Infection (AvgETI) versus actual time of infection by fragment for each APD category.
+#'  
+#' @param train_data: Dataframe containing training data with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`, `VL`
+#' @param new_data: Dataframe containing testing data (for prediction) with columns `APD_1`, `APD_5`, `APD_10`, `HXB2nt_start`, `HXB2nt_end`, `Sample`, `ActualTOI..year.`, `Fragment`
+#' @param apd: apd cutoff (options: 1, 5, 10)
+#' @param type: regression model type (options: "LM", "LM_origin", "LAD", "LAD_origin")
+#' 
 
-plot_ETI_TI_regression <- function(train_data, new_data, apd, type, impute) {
+plot_ETI_TI_regression <- function(train_data, new_data, apd, type) {
     # Set title and file name 
-    title_name = title(apd, 'ti', 'eti', type, impute)
-    file_name = file(apd,'ti', 'eti', type, impute)
+    title_name = title(apd, 'ti', 'eti', type)
+    file_name = file(apd,'ti', 'eti', type)
 
-    actual_data = data_clean(train_data, impute)
+    actual_data = data_clean(train_data)
+
     # Subset data by indicated fragment
-    estimate_together = data.table()
-    for (frag in c('F1', 'F2','F3')){
-        estimated_data = predict_infection_time(new_data, apd, frag, '../_ignore/train_data_predictions.csv', type, impute)
-        estimate_together = rbind(estimate_together, estimated_data)
+    if (type == "LM_GEE"){
+        estimated_together = predict_infection_time(new_data, apd, fragment = "all", '../_ignore/train_data_predictions.csv', type)
+    } else{
+        estimate_together = data.table()
+        for (frag in c('F1', 'F2','F3')){
+            estimated_data = predict_infection_time(new_data, apd, frag, '../_ignore/train_data_predictions.csv', type)
+            estimate_together = rbind(estimate_together, estimated_data)
+        }
     }
 
     together = actual_data[estimate_together, on = c('sample', 'fragment', 'time', 'avg_apd1', 'avg_apd5', 'avg_apd10', 'pos_start', 'pos_end')]    
@@ -60,37 +73,37 @@ plot_ETI_TI_regression <- function(train_data, new_data, apd, type, impute) {
     avg_apd = paste0("avg_apd", apd)
     if (type == 'LM'){
         form = 'y ~ x'
-    } else if (type == 'LM_log'){
-        form = 'y ~ log(x)'
-    } else if (type == 'LM_poly'){
-        form = 'y ~ poly(x, 3, raw = TRUE)'
     } else if (type == 'LM_origin'){
         form = 'y ~ x + 0'
     } else {
         form = NULL
     }
-    #
+    
+    # Plot regression between apd and time since infection 
+
     #w <- ggplot(data=together,aes(y = actual_ti,x= together[[avg_apd]]))+geom_point(aes(shape=fragment, color= sample), size = 2.5) + facet_wrap(~fragment) + #ggtitle(paste0("APD vs. Actual TI for APD", apd, " ", type, " Regression")) + geom_smooth(method = "lm", formula = form, color= apd) + stat_regline_equation#(formula = form, size = 5, label.y = 2.5) + stat_cor(method = "pearson", size = 5, label.y = 2.25) +labs(y='Actual Infection Time', x= #'APD', shape = "Sequence Fragment", color = "Patient")+ theme_bw() + theme(text = element_text(size = 18)) 
 
     #print(w)
     #path = paste(paste0("ActualTI_vs_APD_for_APD",apd, "_Infant_Model"),".pdf",sep="")
     #ggsave(path, plot = last_plot())
 
-    for (frag in c('F1', 'F2', 'F3')){
-        model = model_stats(apd = apd, fragment = frag, type = type, impute = FALSE)
-        print(paste0(type, " Model for APD", apd, " and Fragment ", frag, " Summary Statistics:"))
-        if (type == 'LM' | type == 'LM_origin'){
-            print(model$finalModel)
-        } else if (type == 'LAD' | type == 'LAD_origin'){
-            print(model)
-        }
-        print(summary(model))
-    }
+    # Print model statistics: 
+
+    #for (frag in c('F1', 'F2', 'F3')){
+    #    model = model_stats(apd = apd, fragment = frag, type = type)
+    #    print(paste0(type, " Model for APD", apd, " and Fragment ", frag, " Summary Statistics:"))
+    #    if (type == 'LM' | type == 'LM_origin'){
+    #        print(model$finalModel)
+    #    } else if (type == 'LAD' | type == 'LAD_origin'){
+    #        print(model)
+    #    }
+    #    print(summary(model))
+    #}
 
     # Perfect correlation (ETI = Actual TI) function: 
     func <- function(x){x}
 
-    # Create plot
+    # Create plot comparing estimated time since infection and actual time since infection with a regression line for all patients together
     p <- ggplot(data=together,aes(y = estimated_ti,x= actual_ti))+geom_point(aes(shape=fragment, color= sample), size = 2.5) + facet_wrap(~fragment) + stat_smooth(method="lm", se=TRUE, color= apd, fullrange=TRUE) + ggtitle(title_name) + labs(x='Actual time since infection', y= 'Estimated time since Infection', shape = "Sequence Fragment", color = "Patient")+ stat_regline_equation(size = 5, label.y = 2.5) + stat_cor(method = "pearson", size = 5, label.y = 2.25) + stat_function(fun = func, color = "blue", linetype="dashed", size=1) + theme_bw() + theme(text = element_text(size = 18)) + xlim(-0.25, 2.75) + ylim(-0.25, 2.75)
 
     print(p)
@@ -98,15 +111,20 @@ plot_ETI_TI_regression <- function(train_data, new_data, apd, type, impute) {
     path = paste(file_name,".pdf",sep="")
     ggsave(path, plot = last_plot())
 
+
+    # Create plot comparing estimated time since infection and actual time since infection with regression lines by patient
     a <- ggplot(data=together,aes(y = estimated_ti,x= actual_ti, group = sample, color = sample))+geom_point(aes(shape=fragment, color= sample), size = 2.5) + facet_wrap(~fragment) + stat_smooth(method="lm", se=FALSE, fullrange=FALSE) + ggtitle(title_name) + labs(x='Actual time since infection', y= 'Estimated time since Infection', shape = "Sequence Fragment", color = "Patient")+ stat_function(fun = func, color = "blue", linetype="dashed", size=1) + theme_bw() + theme(text = element_text(size = 18)) + xlim(-0.25, 2.75) + ylim(-0.25, 2.75)
 
     print(a)
 
+
+    # Create density plot comparing the Distribution of the Estimation Error by APD for each fragment
     together$time_difference = together$estimated_ti - together$actual_ti
     z <- ggplot(together, aes(x=time_difference, fill=fragment)) + facet_wrap(~fragment) + geom_histogram(alpha=0.8, position = 'identity', bins = 10) + labs(x = "ETI-TI [years]", fill = "Fragment") + ggtitle(paste0("Distribution of the Estimation Error by APD", apd, " Model"))+ theme_bw() +  theme(text = element_text(size = 18))
     
     print(z)
 
+    # Create line plot comparing the Estimation Error Dependence on Time Since Infection (TI) by APD for each fragment
     together$time_difference_abs = abs(together$time_difference)
     together2 = together[, mean(time_difference_abs), by = .(actual_ti, fragment)]
     colnames(together2) = c("actual_ti", "fragment", "mean_time_difference_abs")
