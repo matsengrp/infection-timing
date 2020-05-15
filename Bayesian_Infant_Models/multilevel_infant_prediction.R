78bc3803475514af85ca1ef8979e4688d1e0cd68
Magdalena Russell <magdalenarussell23@gmail.com>
2020-05-15 08:43:54 -0600

    fixing existing multilevel models

--- Bayesian_Infant_Models/multilevel_infant_prediction.R
+++ Bayesian_Infant_Models/multilevel_infant_prediction.R
@@ -7,7 +7,7 @@ library(RColorBrewer)
 
 ## PREDICTIONS with training data
 # create own link
-posterior_link <- function(apd, frag_index, subject_index, posterior, type){
+posterior_link_existing_data <- function(apd, frag_index, subject_index, posterior, type){
     if (type == "subject_varying_intercepts"){
         time <- with(posterior, population_avg_intercept + subject_intercept[subject_index] + population_avg_slope*apd)
     } else if (type == "subject_fragment_varying_intercepts"){
@@ -22,7 +22,7 @@ posterior_link <- function(apd, frag_index, subject_index, posterior, type){
     return(time)
 }
 
-compute_predictions <- function(model_data, type, posterior){
+compute_predictions_existing_data <- function(model_data, type, posterior){
     prediction_infant_data_cleaned = data.frame()
     for (samp in unique(model_data$subject_index)){
         infant_data_cleaned1 = NULL
@@ -32,7 +32,7 @@ compute_predictions <- function(model_data, type, posterior){
             apd = NULL
             infant_data_cleaned2 = infant_data_cleaned1[with(infant_data_cleaned1, frag_index == frag)]
             apd = infant_data_cleaned2$apd
-            pred.raw <- sapply(1:length(apd), function(i) posterior_link(apd[i], frag, samp, posterior, type))
+            pred.raw <- sapply(1:length(apd), function(i) posterior_link_existing_data(apd[i], frag, samp, posterior, type))
             pred.p <- apply(pred.raw, 2, mean)
             pred.p.PI <- apply(pred.raw, 2, PI)
             infant_data_cleaned2$pred_time = pred.p
@@ -40,4 +40,23 @@ compute_predictions <- function(model_data, type, posterior){
         }   
     }
     return(prediction_infant_data_cleaned)
+}
+
+simulate_apd_time = function(model, i, apd){
+    post <- extract.samples(get(model))
+    
+    if (model == "multilevel_subject_var_int_model") {
+        simulate_subject = rnorm(1,post$subject_mean[i], post$subject_sd[i])
+        time = post$population_avg_intercept[i] + simulate_subject + post$population_avg_slope[i] * apd
+    } else if (model == "multilevel_subject_var_int_fragment_var_slope_model"){
+        simulate_subject = rnorm(1,post$subject_mean[i], post$subject_sd[i])
+        simulate_fragment = rnorm(1,post$fragment_mean[i], post$fragment_sd[i])
+        time = post$population_avg_intercept[i] + simulate_subject + (simulate_fragment + post$population_avg_slope[i]) * apd
+    } else if(model == "multilevel_subject_var_int_fragment_subject_var_slope_model"){
+        simulate_slope_subject = rnorm(1,0, post$subject_sd[i,2])
+        simulate_intercept_subject = rnorm(1,0, post$subject_sd[i,1])
+        simulate_fragment = rnorm(1,post$fragment_slope_mean[i], post$fragment_slope_sd[i])
+        time = post$population_avg_intercept[i] + simulate_intercept_subject + (simulate_slope_subject + simulate_fragment + post$population_avg_slope[i]) * apd
+    } 
+    return(time)
 }
