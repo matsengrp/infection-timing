564d57edabe51b918b69d3ac4ac744869c5b1529
Magdalena Russell <magdalenarussell23@gmail.com>
2020-06-02 08:52:37 -0600

    starting to play with a nonlinear model

--- Bayesian_Infant_Models/stan_models/run_stan_file.R
+++ Bayesian_Infant_Models/stan_models/run_stan_file.R
@@ -45,6 +45,16 @@ stan_time_correction_varying_slopes_frag_subject_more_restricted <- stan(
   control = list(adapt_delta = 0.99, max_treedepth = 20), 
 )
 
+stan_time_correction_non_linear <- stan(
+  file = "multilevel_infant_model_time_error_non_linear.stan",  # Stan program
+  data = data,    # named list of data
+  chains = 4,             # number of Markov chains
+  warmup = 10000,          # number of warmup iterations per chain
+  iter = 20000,            # total number of iterations per chain
+  cores = 2,              # number of cores (could use one per chain)
+  control = list(adapt_delta = 0.99, max_treedepth = 20), 
+)
+
 setwd("/Users/magdalenarussell/Documents/Matsen_group/infection-timing/Bayesian_Infant_Models/")
 save_models("stan_time_correction_varying_slopes_frag_subject")
 
