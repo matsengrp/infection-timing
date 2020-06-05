f49f0d35afc6a986c1fd24f315a99f43a09a713a
Magdalena Russell <magdalenarussell23@gmail.com>
2020-06-05 09:29:16 -0600

    adding model summary and questions, model changes

--- Bayesian_Infant_Models/stan_models/run_stan_file.R
+++ Bayesian_Infant_Models/stan_models/run_stan_file.R
@@ -5,13 +5,13 @@ source("multilevel_infant_functions.R")
 setwd("/Users/magdalenarussell/Documents/Matsen_group/infection-timing/Bayesian_Infant_Models/stan_models")
 
 data <- list(
-    N = nrow(indexed_infant_data_cleaned_subset),
-    N_subject_index = length(unique(indexed_infant_data_cleaned_subset$subject_index)),
-    N_fragment_index = length(unique(indexed_infant_data_cleaned_subset$frag_index)),
-    time = c(indexed_infant_data_cleaned_subset$time),
+    observation_count = nrow(indexed_infant_data_cleaned_subset),
+    subject_count = length(unique(indexed_infant_data_cleaned_subset$subject_index)),
+    fragment_count = length(unique(indexed_infant_data_cleaned_subset$frag_index)),
+    observed_time = c(indexed_infant_data_cleaned_subset$time),
     apd = c(indexed_infant_data_cleaned_subset$apd), 
-    subject_index = c(indexed_infant_data_cleaned_subset$subject_index), 
-    fragment_index = c(indexed_infant_data_cleaned_subset$frag_index)
+    subject = c(indexed_infant_data_cleaned_subset$subject_index), 
+    fragment = c(indexed_infant_data_cleaned_subset$frag_index)
 )
 
 
@@ -52,7 +52,7 @@ stan_time_correction_non_linear <- stan(
   warmup = 10000,          # number of warmup iterations per chain
   iter = 20000,            # total number of iterations per chain
   cores = 2,              # number of cores (could use one per chain)
-  control = list(adapt_delta = 0.99, max_treedepth = 20), 
+  control = list(adapt_delta = 0.99, max_treedepth = 15), 
 )
 
 setwd("/Users/magdalenarussell/Documents/Matsen_group/infection-timing/Bayesian_Infant_Models/")
