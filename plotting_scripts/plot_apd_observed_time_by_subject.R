4bc6f05c0060a0628f4750ea390db72ff5bffc5c
magdalenarussell <magdalenarussell23@gmail.com>
2021-10-13 13:38:19 -0700

    adding apd versus observed time plot

---
+++ plotting_scripts/plot_apd_observed_time_by_subject.R
@@ -0,0 +1,22 @@
+library(rstan)
+library(data.table)
+library(tidyverse)
+library(RColorBrewer)
+library(ggplot2)
+library(cowplot)
+
+args = commandArgs(trailingOnly = TRUE)
+
+TIME_CORRECTION_TYPE <<- args[1]
+stopifnot(TIME_CORRECTION_TYPE %in% c('uniform', 'beta', 'none'))
+NCPU <<- 2
+PREPROCESS_DATA <<- TRUE
+
+source('config/config.R')
+source(paste0(PROJECT_PATH, '/config/file_paths.R'))
+source(paste0(PROJECT_PATH, '/scripts/model_fitting_functions.R'))
+source(paste0(PROJECT_PATH, '/plotting_scripts/plotting_functions.R'))
+
+infant_data = as.data.table(configure_data(TRAINING_INFANT_DATA_PATH))
+
+plot_apd_time_by_subject(together)
