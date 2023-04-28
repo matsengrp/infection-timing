e6ba0de5ead596461767b65bd5df16b5b366f2c7
Maggie Russell <56004363+magdalenarussell@users.noreply.github.com>
2023-04-28 16:01:59 -0700

    Dev (#4)
    * adding Pritha's processing script
    
    * updating plot for ms
    
    * starting to look at CD4 counts
    
    * updating cd4 plot

--- config/file_paths.R
+++ config/file_paths.R
@@ -1,3 +1,4 @@
 TRAINING_INFANT_DATA_PATH <<- paste0(PROJECT_PATH, '/_ignore/runs1_7_apd_vload_subtype_20220510.csv')
+TRAINING_INFANT_METADATA_PATH <<- paste0(PROJECT_PATH, '/_ignore/infant_cd4.txt')
 TESTING_INFANT_DATA_PATH  <<- paste0(PROJECT_PATH, '/_ignore/run8_testing_samples_apd_subtype_20220804.csv')
 TESTING_INFANT_DATA_TRUE_TIME_PATH <<- paste0(PROJECT_PATH, '/_ignore/run8_testing_samples_actualTOI_082022.csv')
